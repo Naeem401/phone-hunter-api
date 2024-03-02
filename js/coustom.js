b2a57a0c2e74cf4e9cb3e28666eda56e6d7404e0
Md Naeem Mia <mdnaeemm401@gmail.com>
2024-03-02 03:49:17 +0300

    js complet

--- js/coustom.js
+++ js/coustom.js
@@ -1,4 +1,4 @@
-const loadPhones = async(getSearchText, isShowMore) => {
+const loadPhones = async(getSearchText='13', isShowMore) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${getSearchText}`;
     const res = await fetch(url);
     const data = await res.json();
@@ -60,7 +60,36 @@ const showDitailHendel = async(id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data)
+    const phoneDetails = data.data;
+    showPhoneDetails(phoneDetails);
+};
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    <dialog id="my_modal" class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+      <div class="flex justify-center bg-[#0D6EFD0D] p-12 rounded-lg"><img src="${phone.image}" alt=""></div>
+      <h3 class="poppins font-semibold text-3xl leading-9 mt-8 text-[#403F3F]">Iphone 13 Pro Max</h3>
+     <p class="poppins font-normal text-base leading-6 tracking-normal text-[#706F6F] mt-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+     <h4><span class="poppins font-semibold text-lg leading-tight">Storage :</span> ${phone?.mainFeatures?.storage}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Display Size :</span>${phone?.mainFeatures?.displaySize}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Chipset :</span>${phone?.mainFeatures?.chipSet}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Memory :</span>${phone?.mainFeatures?.memory}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Slug : </span>${phone?.slug}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Release data :</span> ${phone?.releaseDate}</h4>
+     <h4><span class="poppins font-semibold text-lg leading-tight>Brand :</span>${phone.brand}</h4>
+     <h4><span class="font-bold">GPS :</span>${phone?.others?.GPS}</h4>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog> `
+  my_modal.showModal();
 }
 
 
